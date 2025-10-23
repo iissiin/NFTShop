78a46c1e8d3c94b44c4e05ff0c2a88295a8c1078
iissiin <issina080208@gmail.com>
2025-10-23 22:33:51 +0500

    Initial commit

---
+++ NFTShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NFTShop
+//
+//  Created by Дария Исина on 23.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NFTShopTests/NFTShopTests.swift
@@ -0,0 +1,17 @@
+//
+//  NFTShopTests.swift
+//  NFTShopTests
+//
+//  Created by Дария Исина on 23.10.2025.
+//
+
+import Testing
+@testable import NFTShop
+
+struct NFTShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
